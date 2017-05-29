4e5b87b6860f70d0494c0575d2815d36d7676263
Eric Tan <ejtan@ucdavis.edu>
2017-05-28 22:31:07 -0700

    Added class methods. Not implamented

--- include/neighbor_table.h
+++ include/neighbor_table.h
@@ -1,11 +1,16 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+#include <vector>
+
 
 /* Class : Neighbor_table
  *
  * Provides a neighbor table used to access neighbors of a given site in
  * a lattice. A square lattice is the only available in both 2 and 3 dimensions.
+ * The table is set as a 1D vector with elements accessed by
+ * x site * number of neighbors + i-th neighbor.
+ *
  * Table follows the convention:
  *
  * 2D:
@@ -20,9 +25,17 @@
 class Neighbor_table
 {
     private:
+        int size, n_neigh;
+        std::vector<int> table;
+        void set_2D_table(const int L);
+        void set_3D_table(const int L);
 
     public:
         Neighbor_table();
+        Neighbor_table(const int L, const int dim);
+        void init(const int L, const int dim);
+        int operator[](const int idx);
+        std::vector<int> get_table();
 };
 
 
--- src/neighbor_table.cpp
+++ src/neighbor_table.cpp
@@ -1,5 +1,62 @@
 #include "../include/neighbor_table.h"
 
+
+/*-----------------------------------------------------------------------------
+ * PRIVATE METHODS
+ *---------------------------------------------------------------------------*/
+
+/* set_2D_table()
+ * Sets the neighbor table as a 2D square lattice.
+ */
+void set_2D_table(const int L)
+{
+}
+
+
+/* set_2D_table()
+ * Sets the neighbor table as a 3D square lattice.
+ */
+void set_3D_table(const int L)
+{
+}
+
+/*-----------------------------------------------------------------------------
+ * PUBLIC METHODS
+ *---------------------------------------------------------------------------*/
+
+/* Default constructor.
+ */
 Neighbor_table::Neighbor_table()
+{
+}
+
+
+/* Constructor with initalizer list.
+ */
+Neighbor_table::Neighbor_table(const int L, const int dim)
+{
+}
+
+
+/* init()
+ * Initalizes neighbor table like the constructor with initalizer list.
+ */
+void Neighbor_table::init(const int L, const int dim)
+{
+}
+
+
+/* operator[]
+ * Returns the input index of the array.
+ */
+int Neighbor_table::operator[](const int idx)
+{
+}
+
+
+/* get_table()
+ * Outputs the neighbor table vector.
+ */
+std::vector<int> Neighbor_table::get_table()
 {
 }
