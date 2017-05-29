00fd6ba6bb0ca67407f1921dee2cdff28d8dd2ef
Eric Tan <ejtan@ucdavis.edu>
2017-05-28 22:05:44 -0700

    Added neighbor_table class

---
+++ include/neighbor_table.h
@@ -0,0 +1,29 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+
+/* Class : Neighbor_table
+ *
+ * Provides a neighbor table used to access neighbors of a given site in
+ * a lattice. A square lattice is the only available in both 2 and 3 dimensions.
+ * Table follows the convention:
+ *
+ * 2D:
+ *      0
+ *      |
+ *  3 - x - 1
+ *      |
+ *      2
+ *
+ * 3D: Do later.
+ */
+class Neighbor_table
+{
+    private:
+
+    public:
+        Neighbor_table();
+};
+
+
+#endif
---
+++ src/neighbor_table.cpp
@@ -0,0 +1,5 @@
+#include "../include/neighbor_table.h"
+
+Neighbor_table::Neighbor_table()
+{
+}
