85361602705ac3e81cea5e222f984eaea5aa2940
Eric Tan <ejtan@ucdavis.edu>
2017-05-31 05:32:53 -0700

    Renamed file

--- Makefile
+++ Makefile
@@ -3,14 +3,16 @@ CFLAGS = -pipe -O2 -std=c++14 -march=native
 WARN = -Wall -Werror -Wfloat-equal -ansi -pedantic
 OBJ = exchange_table.o neighbor_table.o
 
-test_neigh.out: neighbor_table.o test_neigh.o
-	$(CC) $(WARN) $(CFLAGS) neighbor_table.o test_neigh.o -o test_neigh.out
+
+test.out: neighbor_table.o test.o
+	$(CC) $(WARN) $(CFLAGS) neighbor_table.o test.o -o bin/test.out
+
 
 neighbor_table.o: src/neighbor_table.cpp include/neighbor_table.h
 	$(CC) $(WARN) $(CFLAGS) -c src/neighbor_table.cpp
 
-test_neigh.o: test/test_neigh.cpp
-	$(CC) $(WARN) $(CFLAGS) -c test/test_neigh.cpp
+test.o: test/test.cpp
+	$(CC) $(WARN) $(CFLAGS) -c test/test.cpp
 
 clean:
-	rm -Rf *.out *.o
+	rm -Rf *.o
--- test/test.cpp
+++ test/test.cpp
@@ -10,15 +10,20 @@
 #include <algorithm>
 #include "../include/neighbor_table.h"
 
-
-int main(void)
+/* test_neighbor()
+ *
+ * Compares output of a 2D 3x3 lattice with on computed by hand following
+ * the convention set in include/neighbor_table.h
+ */
+void test_neighbor()
 {
     Neighbor_table neigh(3, 2);
+
+    // Hard coded hand computed neighbor table.
     std::vector<int> actual_2D = {6, 1, 3, 2, 7, 2, 4, 0, 8, 0, 5, 1, 0, 4, 6, 5,
         1, 5, 7, 3, 2, 3, 8, 4, 3, 7, 0, 8, 4, 8, 1, 6, 5, 6, 2, 7};
 
     if (neigh.size() != actual_2D.size()) {
-        std::cout << neigh.size() << ' ' << actual_2D.size() << std::endl;
         std::cerr << "Error: Expected test and neighbor table to be same size"
             << std::endl;
         exit(EXIT_FAILURE);
@@ -26,10 +31,16 @@ int main(void)
 
     std::cout << "Testing if generated neighbor table is correct...\n\n";
 
+    // Check if site table are equal
     if (std::equal(actual_2D.begin(), actual_2D.end(), neigh.begin()))
         std::cout << "2D Neighbor Table Test Passed.\n";
     else
         std::cout << "2D Neighbor Table Test Failed.\n";
+}
+
+int main(void)
+{
+    test_neighbor();
 
     return 0;
 }
