6e4c9072d4d8a6bb511eb0878852670b44232123
Eric Tan <ejtan@ucdavis.edu>
2017-06-01 01:42:59 -0700

    Added exchange table test

--- test/test.cpp
+++ test/test.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <cmath>
 #include "../include/neighbor_table.h"
 #include "../include/exchange_table.h"
 
@@ -41,10 +43,75 @@ void test_neighbor()
 }
 
 
+/* check_exchange()
+ *
+ * Checks if connecting bonds are the same. Takes the difference and checks if
+ * the absolute value is less than machine percision.
+ */
+bool check_exchange(const Exchange_table &J, const Neighbor_table &neigh)
+{
+    double bond1, bond2;
+
+    for (int i = 0; i < 9; i++) {
+        bond1 = J[i * 4];
+        bond2 = J[neigh[i * 4] * 4 + 2];
+
+        if (fabs(bond1 - bond2) > std::numeric_limits<double>::epsilon())
+            return false;
+
+        bond1 = J[i * 4 + 1];
+        bond2 = J[neigh[i * 4 + 1] * 4 + 3];
+
+        if (fabs(bond1 - bond2) > std::numeric_limits<double>::epsilon())
+            return false;
+    }
+
+    return true;
+}
+
+
+/* test_exchange()
+ *
+ * Compares the bonds i - j to check if they are the same.
+ */
+void test_exchange()
+{
+    Exchange_table J(3, 2);
+    Neighbor_table neigh(3, 2);
+
+    std::cout << "Testing if continuous exchange table is correctly...\n";
+    for (int i = 0; i < 5; i++) {
+        std::cout << "  Performing Test " << i + 1 << "... ";
+
+        J.generate_continuous(5.0);
+        if (check_exchange(J, neigh))
+            std::cout << "2D Exchange Table Test Passed.\n";
+        else
+            std::cout << "2D Exchange Table Test Failed.\n";
+    }
+
+    std::cout << "Testing if discrete exchange table is correctly...\n";
+    for (int i = 0; i < 5; i++) {
+        std::cout << "  Performing Test " << i + 1 << "... ";
+
+        std::uniform_real_distribution<double> rand0(0.0, 1.0);
+        std::random_device rd;
+        std::mt19937 engine(rd());
+
+        J.generate_discrete(1.0, rand0(engine));
+        if (check_exchange(J, neigh))
+            std::cout << "2D Exchange Table Test Passed.\n";
+        else
+            std::cout << "2D Exchange Table Test Failed.\n";
+    }
+}
+
+
 int main(void)
 {
     std::cout << "Performing Tests\n\n";
     test_neighbor();
+    test_exchange();
 
     return 0;
 }
