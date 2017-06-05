0054ac7677bf7b1c9d95cb03adacd0f0a1ba9bb5
Eric Tan <ejtan@ucdavis.edu>
2017-06-04 23:25:03 -0700

    Added set_spin()

--- src/ising.cpp
+++ src/ising.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../include/ising.h"
 
 
@@ -12,7 +13,7 @@ Ising::Ising() : rand0(0.0, 1.0)
  */
 Ising::Ising(const int L, const int dim) : Model(L, dim), rand0(0.0, 1.0)
 {
-    spin.reserve(size * n_neigh);
+    spin.reserve(size);
 }
 
 
@@ -23,16 +24,23 @@ Ising::Ising(const int L, const int dim) : Model(L, dim), rand0(0.0, 1.0)
 void Ising::init(const int L, const int dim)
 {
     Model::init(L, dim);
-    spin.reserve(size * n_neigh);
+    spin.reserve(size);
 }
 
 
 /* set_spin()
  *
- * Sets the spin of lattice. Initalizes to +/- 1.
+ * Sets the spin of lattice. Initalizes to +1.
  */
 void Ising::set_spin()
 {
+    if (size) {
+        for (int i = 0; i < size; i++)
+            spin[i] = 1;
+    } else {
+        std::cerr << "Ising class is not initalized" << std::endl;
+        exit(EXIT_FAILURE);
+    } // Check if Class is initalized
 }
 
 
