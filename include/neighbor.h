31221856cfc9df09715ad87d3e850f89fb1dde4d
Eric Tan <ejtan@ucdavis.edu>
2017-07-10 23:18:34 -0700

    Added Neighbor class

--- include/neighbor.h
+++ include/neighbor.h
@@ -26,14 +26,47 @@
 template <std::size_t Dim>
 struct Neighbor
 {
-    static constexpr N = Dim * 2;
-    std::array<int, N> neighbor;
+    std::array<int, Dim * 2> neighbor;
+    
+    /* Due to the way C++ handles compiling template structs/classes, the implamentation is
+     * here instead of two seperate files (Check the best way to handle this later).
+     */
+    Neighbor() = default;
 
-    Neighbor();
-    Neighbor(const Neighbor &rhs);
-    set_neighbors(int pos);
+    Neighbor(const Neighbor<Dim> &rhs) : neighbor(rhs.neighbor) {}
+
+    void set_neighbors(int pos, int L)
+    {
+        /* In C++17 standard, replace if statements with if constexpr(...)
+         */
+        if (Dim == 2 || Dim == 3) {
+            int N2 = L * L; // Number of sites on a 2D layer
+
+            // 0-th neighbor
+            if ((pos % N2) - L < 0)   neighbor[0] = pos + N2 - L;
+            else                      neighbor[0] = pos - L;
+            // 1-st neighbor
+            if ((pos + 1) % L == 0)   neighbor[1] = pos + 1 - L;
+            else                      neighbor[1] = pos + 1;
+            // 2-nd neighbor
+            if ((pos + L) % N2 >= N2) neighbor[2] = pos + L - N2;
+            else                      neighbor[2] = pos + L;
+            // 3-rd neighbor
+            if (pos % L == 0)         neighbor[3] = pos + L - 1;
+            else                      neighbor[3] = pos - 1;
+
+            if (Dim == 3) {
+                int N3 = L * L * L; // Total number of sites in a 3D lattice
+
+                // 4-th neighbor
+                if (pos - N2 < 0)  neighbor[4] = pos + N3 - N2;
+                else               neighbor[4] = pos - N2;
+                // 5-th neighbor
+                if (pos + N2 > N3) neighbor[5] = pos - N3 + N2;
+                else               neighbor[5] = pos + N2;
+            } // Set 3D layer (if needed)
+        } // Set 2D layer
+    }
 };
-
-#include "../src/neighbor.cpp"
 
 #endif
