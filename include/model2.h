1a0ffed80846a934e58e969ecdd1b52372b5686a
Eric Tan <ejtan@ucdavis.edu>
2017-07-13 01:52:25 -0700

    Added virtual set_spin()

--- include/model2.h
+++ include/model2.h
@@ -26,6 +26,7 @@ class Model2
         Model2();
         Model2(const int L);
         Model2(const Model2 &rhs);
+        virtual void set_spin() = 0;
         void set_exchange(double delta);
         std::vector<Exchange<2>> get_exchange() const;
 };
