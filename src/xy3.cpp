edbaaf27511159eabfc1d8d4e32625bca6c8f6b9
Eric Tan <ejtan@ucdavis.edu>
2017-08-17 03:49:56 -0700

    Added 3D XY model

---
+++ src/xy3.cpp
@@ -0,0 +1,37 @@
+#include "../include/xy3.h"
+
+
+/*-------------------------------------------------------------------------------------------------
+ * PUBLIC METHODS
+ *-----------------------------------------------------------------------------------------------*/
+
+/* Constructor with arguments
+ */
+XY3::XY3(const int L) : Clock3(L, 50)
+{
+}
+
+
+/* Copy constructor
+ */
+XY3::XY3(const XY3 &rhs) : Clock3(rhs)
+{
+}
+
+
+/* set_spin()
+ * Calss clock model set_spin()
+ */
+void XY3::set_spin()
+{
+    Clock3::set_spin();
+}
+
+
+/* sweep_energy()
+ * Calls clock mode sweep_energy()
+ */
+double XY3::sweep_energy(double beta, std::mt19937 &engine)
+{
+    return Clock3::sweep_energy(beta, engine);
+}
