0826d75f421eddd018f5e914353f59346e689c17
Eric Tan <eric@localhost.localdomain>
2017-12-17 10:16:12 -0800

    Implamented constructors

--- src/data_matrix.cpp
+++ src/data_matrix.cpp
@@ -1,1 +1,24 @@
 #include "../include/data_matrix.h"
+
+
+/* Default constructor
+ */
+Data_matrix::Data_matrix() : N_row(0), N_col(0), filled_row(0), data(nullptr)
+{
+}
+
+
+/* Constructor with arguments
+ */
+Data_matrix::Data_matrix(size_t r, size_t c) : N_row(r), N_col(c), filled_row(0)
+{
+    data = new double[r * c];
+}
+
+
+/* Copy constructor
+ */
+Data_matrix::Data_matrix(const Data_matrix &rhs) :
+    N_row(rhs.N_row), N_col(rhs.N_col), filled_row(rhs.filled_row), data(rhs.data)
+{
+}
