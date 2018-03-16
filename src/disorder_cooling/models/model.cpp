ec51f9ea190ef93c12552b6027c819a967ff1db1
Eric Tan <eric@thinkpad.attlocal.net>
2018-03-16 02:15:21 -0700

    Added base class

---
+++ src/disorder_cooling/models/model.cpp
@@ -0,0 +1,62 @@
+template <int Dim, int L>
+Model<Dim, L>::Model() : isClean(true), rand0(0.0, 1.0)
+{
+    size = L * L;
+    neigh.resize(size);
+    J.resize(size);
+
+    for (int i = 0; i < size; ++i)
+        neigh[i].set_neighbors(i);
+}
+
+
+template <int Dim, int L>
+Model<Dim, L>::Model(const Model &rhs) :
+    size(rhs.size), isClean(rhs.isClean), neigh(rhs.neigh), J(rhs.J)
+{
+}
+
+
+
+template <int Dim, int L>
+void Model<Dim, L>::set_exchange(double delta)
+{
+    if (isClean)
+        isClean = false;
+
+    std::random_device rd;
+    std::mt19937 engine(rd());
+
+    for (int i = 0; i < size; i++) {
+        // 0 - 2 bond
+        r_val = rand0(engine);
+        if (rand0(engine) > 0.5) J_val = 1.0 - (delta * r_val / 2.0);
+        else                     J_val = 1.0 + (delta * r_val / 2.0);
+        J[i].J_arr[0]                    = J_val;
+        J[neigh[i].neighbor[0]].J_arr[2] = J_val;
+
+        // 1 - 3 bond
+        r_val = rand0(engine);
+        if (rand0(engine) > 0.5) J_val = 1.0 - (delta * r_val / 2.0);
+        else                     J_val = 1.0 + (delta * r_val / 2.0);
+        J[i].J_arr[1]                    = J_val;
+        J[neigh[i].neighbor[1]].J_arr[3] = J_val;
+
+        if (Dim == 3) {
+            // 4 - 5 bond
+            r_val = rand0(engine);
+            if (rand0(engine) > 0.5) J_val = 1.0 - (delta * r_val / 2.0);
+            else                     J_val = 1.0 + (delta * r_val / 2.0);
+            J[i].J_arr[4]                    = J_val;
+            J[neigh[i].neighbor[4]].J_arr[5] = J_val;
+        } // Set 3D bond if needed
+    } // Set bond
+}
+
+
+template <int Dim, int L>
+void Model<Dim, L>::set_run_param(int w, int m)
+{
+    warmup = w;
+    measure = m;
+}
---
+++ src/disorder_cooling/models/model.h
@@ -0,0 +1,83 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include <vector>
+#include <random>
+#include "../tables/neighbor.h"
+#include "../tables/exchange.h"
+
+
+/* class: Model
+ *
+ * @template: Dim = number of dimensions
+ * @template: L = number of lattice sites in a dimension
+ *
+ * Base class for 2D classical spin models.
+ */
+template <int Dim, int L>
+class Model
+{
+    static_assert(Dim == 2 || Dim == 3, "Error: Dim must be 2 or 3.");
+    static_assert(L > 0, "Error: L must be positive and nonzero.");
+
+    protected:
+        int warmup = 30000;
+        int measure = 500000;
+        int size;
+        bool isClean;
+        std::uniform_real_distribution<float> rand0;
+        std::vector< Neighbor<Dim, L> > neigh;
+        std::vector< Exchange<Dim, L> > J;
+
+    public:
+        /* Constructors
+         */
+        Model();
+        Model(const Model &rhs);
+
+        /* virtual set_spin()
+         *
+         * Initalizes the spins in the system.
+         */
+        virtual void set_spin() = 0;
+
+        /* virtual sweep_energy()
+         *
+         * @param: beta = 1 / T
+         * @param: engine = mt19937 random number generator
+         *
+         * @return: double representing the energy at beta.
+         */
+        virtual void sweep_energy(double beta, std::mt19937 &engine) = 0;
+
+        /* virtual sweep_binder()
+         *
+         * @param: beta = 1 / T
+         * @param: engine = mt19937 random number generator
+         *
+         * @return: double representing the binder ratio at beta.
+         */
+        virtual void sweep_binder(double beta, std::mt19937 &engine) = 0;
+
+        /* set_exchange()
+         *
+         * @param: delta = width of the uniform distribution to pick disorders from, centered at 1.
+         *
+         * Sets the bond disorder.
+         */
+        void set_exchange(double delta);
+
+        /* set_run_param()
+         *
+         * @param: w = warmup steps
+         * @param: m = measure steps
+         *
+         * Overwrites the default initalized parameters.
+         */
+        void set_run_param(int w, int m);
+};
+
+
+#include "model.cpp"
+
+#endif
